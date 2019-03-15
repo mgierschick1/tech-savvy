73eb5ace95198d6a57153e5c7a2bbdb6951a5fe4
mgierschick1 <mgierschick1@babson.edu>
2019-03-15 16:22:28 -0400

    finished class day0

---
+++ calc.py
@@ -0,0 +1,59 @@
+# print((3+15+2+43)*2019)
+#
+# # print(2019-2000)
+# # print("hello, world!")
+# #
+# # print(2**100)
+#
+# print("hello, world!")
+# print(-2**100)
+# print ( -2*100)
+#
+# print('Hey Jude')
+#
+# print('1 + 2 + 3 =' , 1+2+3)
+#
+# # name = input()
+#
+# print(name)
+# # name = input()
+# # print('Hello, ', name)
+# # print('Hello, ', int(name))
+# # n=100
+# # print(n)
+#
+#
+# first_name = 'John'
+# # last_name = 'Lennon'
+#
+# # print(first_name, last_name)
+#
+# print('hello, {}! You are {} years old.'.format(first_name, 20))
+#
+# template = 'hello, {}! You are {} years old.'
+#
+# name1 = 'Grace'
+# age1 = 20
+#
+# name2 = 'Aida'
+# age2 = 18
+#
+# print(template.format(name1, age1))
+#
+# print(template.format(name2, age2))
+
+import math
+
+print(math.pow(2, 5))
+print(math.pi)
+
+
+
+
+
+
+
+
+
+
+
