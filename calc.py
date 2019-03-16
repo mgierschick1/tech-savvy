668464cbffa127c884ca9f78d3dd500c9fbf8358
mgierschick1 <mgierschick1@babson.edu>
2019-03-16 15:12:28 -0400

    day 1 (Saturday)

--- calc.py
+++ calc.py
@@ -1,4 +1,4 @@
-# print((3+15+2+43)*2019)
+print((3+15+2+43)*2019)
 #
 # # print(2019-2000)
 # # print("hello, world!")
@@ -42,18 +42,43 @@
 #
 # print(template.format(name2, age2))
 
-import math
+# import math
+#
+# print(math.pow(2, 5))
+# print(math.pi)
+#
+# print(3+15+2+43)
+# # '3+15+2+43'
+# print('Hello, world!')
+# print('3=6-9=10')
+# print(46 + 37 + 38)
+# print(15 / 3)
 
-print(math.pow(2, 5))
-print(math.pi)
+# print(2**6)
+#
+# print(7 5)
 
+# print(42*60+42)
+# print(10/1.61)
+# print(((42*60+42)/(10/1.61))/60)
+# print(60/(((42*60+42)/(10/1.61))/60))
+#
+# n = 50
+# print(n)
 
+# x = 10
+# x = x + 2
+# print(x)
 
+# print('Today is {:2d}-{:02d}.'.format(3, 20))
+#
+NUMBER_OF_SECONDS_IN_ONE_MINUTE = 60
 
+seconds = 42 * NUMBER_OF_SECONDS_IN_ONE_MINUTE + 42
+print("There are {} seconds".format(seconds))
 
+# Exercise 2.2
 
+NUMBER_OF_KILOMETERS_IN_A_MILE = 1.61
 
-
-
-
-
+miles = 10/NUMBER_OF_KILOMETERS_IN_A_MILE
